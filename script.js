297769f04b08eb865925f0084af0aea60e2c4489
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-03 14:50:14 -0300

    I transformed the result into a json() storing it in the json variable.

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ document.querySelector('.busca').addEventListener('submit', async (event)=>{
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=68cba343c18b61137424867abb34b274&units=metric&lang=en`
         
         let results = await fetch(url)
+        let json = await results.json()
+
+        console.log(json)
     }
 })
 
