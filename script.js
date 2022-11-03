b3ef28131a8194009c8ecd044ab6e3172e0465e6
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-03 14:29:23 -0300

    I created the input variable to record the information the user entered and created a showWarning function to load the search.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.querySelector('.busca').addEventListener('submit', (event)=>{
     event.preventDefault()
+
+    let input = document.querySelector('#searchInput').value
+
+    if(input !== ''){
+        showWarning('Loading...')
+    }
 })
+
+function showWarning(msg){
+    document.querySelector('.aviso').innerHTML = msg
+}
