e490359778d7219e9c951eee25e43dc86d5a6cf0
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-03 14:43:32 -0300

    I created the complete url to connect via API with the Open Wheather website and created a variable for the result using the fetch() function.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
     event.preventDefault()
 
     let input = document.querySelector('#searchInput').value
 
     if(input !== ''){
         showWarning('Loading...')
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=68cba343c18b61137424867abb34b274&units=metric&lang=en`
+        
+        let results = await fetch(url)
     }
 })
 
