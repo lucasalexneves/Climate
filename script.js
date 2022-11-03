77db625bb947cb23dd306ba63e93556d9db39856
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-03 15:37:24 -0300

    I created the clearInfo function to clear the showWarning and clear the result. Program finished.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.querySelector('.busca').addEventListener('submit', async (event)=>{
     let input = document.querySelector('#searchInput').value
 
     if(input !== ''){
+        clearInfo()
         showWarning('Loading...')
 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=68cba343c18b61137424867abb34b274&units=metric&lang=en`
@@ -21,15 +22,16 @@ document.querySelector('.busca').addEventListener('submit', async (event)=>{
                 wingAngle: json.wind.deg
             })
         } else {
+            clearInfo()
             showWarning('We could not find this location.')
         }
+    } else{
+        clearInfo()
     }
 })
 
 function showInfo(json){
     showWarning('')
-
-    document.querySelector('.resultado').style.display = 'block'
 
     document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
     document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>` 
@@ -38,6 +40,13 @@ function showInfo(json){
     document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
 
     document.querySelector('.ventoPonto').style.transform = `rotate(${json.wingAngle-90}deg)` 
+
+    document.querySelector('.resultado').style.display = 'block'
+}
+
+function clearInfo(){
+    showWarning('')
+    document.querySelector('.resultado').style.display = 'none'
 }
 
 function showWarning(msg){
